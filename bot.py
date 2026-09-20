d7007347e1f6d1f8b239b9e6fd7b5427ec8a95c6
nova125488me-eng <nova125488me@gmail.com>
2026-09-20 14:01:19 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,9 +22,9 @@ def run_web():
     port = int(os.environ.get("PORT", 8080))
     app_flask.run(host='0.0.0.0', port=port)
 
-# استفاده از مدل جدید و فعال Llama 3.2
+# استفاده از مدل فوق‌العاده پایدار Mixtral
 client = Groq(api_key="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")
-MODEL_NAME = "llama-3.2-3b-preview"
+MODEL_NAME = "mixtral-8x7b-32768"
 
 chat_histories = {}
 
